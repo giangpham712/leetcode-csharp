729fa4bdb01f3449cae172c4f0766d82929c192a
Giang Pham <giang.phamhoang712@gmail.com>
2024-03-28 10:51:49 +1100

    Solution for Merge k Sorted Lists

---
+++ src/ConsoleApp/Problems/LeetCode/MergeKLists/MergeKListsSolution.cs
@@ -0,0 +1,60 @@
+﻿namespace ConsoleApp.Problems.LeetCode.MergeKLists;
+
+public class MergeKListsSolution
+{
+    public ListNode Solve(ListNode[] lists)
+    {
+        var nonEmptyLists = lists.Where(l => l != null).ToList();
+        if (!nonEmptyLists.Any()) {
+            return null;
+        }
+
+        if (nonEmptyLists.Count() == 1) {
+            return nonEmptyLists.First();
+        }
+
+        var allNodes = new List<ListNode>();
+        foreach (var headNode in nonEmptyLists) {
+            var currentNode = headNode;
+            while (currentNode != null) {
+                allNodes.Add(currentNode);
+                currentNode = currentNode.next;
+            }
+        }
+
+        allNodes = allNodes.OrderBy(n => n.val).ToList();
+
+        ListNode prevNode = null;
+        foreach (var node in allNodes) {
+            if (prevNode != null) {
+                prevNode.next = node;
+            }
+
+            prevNode = node;
+        }
+
+        return allNodes.First();
+    }
+}
+
+public class ListNode {
+    public int val;
+    public ListNode next;
+    public ListNode(int val=0, ListNode next=null) {
+        this.val = val; 
+        this.next = next;
+    }
+
+    public int[] ToArray()
+    {
+        var list = new List<int>();
+        var currentNode = this;
+        while (currentNode != null)
+        {
+            list.Add(currentNode.val);
+            currentNode = currentNode.next;
+        }
+
+        return list.ToArray();
+    }
+}
---
+++ src/ConsoleApp/Problems/LeetCode/MergeKLists/MergeKListsTest.cs
@@ -0,0 +1,56 @@
+﻿using FluentAssertions;
+
+namespace ConsoleApp.Problems.LeetCode.MergeKLists;
+
+public class MergeKListsTest
+{
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void TestSolve(int[][] input, int[] output)
+    {
+        var lists = new List<ListNode>();
+
+        foreach (var arr in input)
+        {
+            if (!arr.Any())
+            {
+                lists.Add(null);
+            }
+            else
+            {
+                var head = CreateListNode(arr);
+                lists.Add(head);
+            }
+        }
+        
+        var solution = new MergeKListsSolution();
+        var actual = solution.Solve(lists.ToArray());
+
+        actual.ToArray().Should().BeEquivalentTo(output);
+    }
+
+    private ListNode CreateListNode(int[] arr)
+    {
+        var head = new ListNode();
+        head.val = arr[0];
+        
+        var prevNode = head;
+        for (var i = 1; i < arr.Length; i++)
+        {
+            var nextNode = new ListNode(arr[i]);
+            prevNode.next = nextNode;
+            prevNode = nextNode;
+        }
+
+        return head;
+    }
+    
+    public static IEnumerable<object[]> TestData()
+    {
+        yield return new object[]
+        {
+            new[] { new[] { -2, -2 }, new[] { -3 } },
+            new[] { -3, -2, -2 }
+        };
+    }
+}
