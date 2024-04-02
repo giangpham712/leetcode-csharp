98587c7a4f10c36f34ac8e81868e8fc1eba7dad9
Giang Pham <giang.phamhoang712@gmail.com>
2024-04-03 09:32:32 +1100

    Solution for Number of Islands

---
+++ src/ConsoleApp/Problems/LeetCode/NumIslands/NumIslandsSolution.cs
@@ -0,0 +1,39 @@
+﻿namespace ConsoleApp.Problems.LeetCode.Problem;
+
+public class NumIslandsSolution
+{
+    public int Solve(char[][] grid)
+    {
+        var keys = new HashSet<int>();
+
+        var count = 0;
+        for (var i = 0; i < grid.Length; i++) {
+            for (var j = 0; j < grid[i].Length; j++) {
+                Expand(i, j, grid, count, keys);
+                count++;
+            }    
+        }
+        
+        return keys.Count;
+    }
+    
+    public void Expand(int i, int j, char[][] grid, int key, HashSet<int> keys) 
+    {
+        if (grid[i][j] == '0') return;
+        
+        grid[i][j] = '0';
+        keys.Add(key);
+     
+        if (i > 0)
+            Expand(i - 1, j, grid, key, keys);
+        
+        if (j > 0)
+            Expand(i, j - 1, grid, key, keys);
+        
+        if (i < grid.Length - 1)
+            Expand(i + 1, j, grid, key, keys);
+        
+        if (j < grid[0].Length - 1)
+            Expand(i, j + 1, grid, key, keys);
+    }
+}
---
+++ src/ConsoleApp/Problems/LeetCode/NumIslands/NumIslandsTest.cs
@@ -0,0 +1,43 @@
+﻿using ConsoleApp.Problems.LeetCode.Problem;
+
+namespace ConsoleApp.Problems.LeetCode.NumIslands;
+
+public class NumIslandsTest
+{
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void TestSolve(char[][] input, int expected)
+    {
+        var solution = new NumIslandsSolution();
+        var actual = solution.Solve(input);
+
+        Assert.Equal(expected, actual);
+    }
+
+    private static IEnumerable<object[]> TestData()
+    {
+        yield return new object[]
+        {
+            new char[][]
+            {
+                new[] { '1', '1', '1', '1', '0' }, 
+                new[] { '1', '1', '0', '1', '0' }, 
+                new[] { '1', '1', '0', '0', '0' },
+                new[] { '0', '0', '0', '0', '0' }
+            },
+            1
+        };
+
+        yield return new object[]
+        {
+            new char[][]
+            {
+                new[] { '1', '1', '0', '0', '0' },
+                new[] { '1', '1', '0', '0', '0' },
+                new[] { '0', '0', '1', '0', '0' },
+                new[] { '0', '0', '0', '1', '1' }
+            },
+            3
+        };
+    }
+}
