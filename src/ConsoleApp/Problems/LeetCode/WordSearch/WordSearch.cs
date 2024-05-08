b8129fe572863de94986c58a0f6b616c9975b45a
Giang Pham <giang.phamhoang712@gmail.com>
2024-05-08 23:45:20 +1000

    Solution for Word Search

---
+++ src/ConsoleApp/Problems/LeetCode/WordSearch/WordSearch.cs
@@ -0,0 +1,75 @@
+﻿namespace ConsoleApp.Problems.LeetCode.WordSearch;
+
+public class WordSearch
+{
+    public bool Solve(char[][] board, string word)
+    {
+        var frequencies = new Dictionary<char, int>();
+        for (var i = 0; i < board.Length; i++)
+        {
+            for (var j = 0; j < board[0].Length; j++)
+            {
+                if (!frequencies.ContainsKey(board[i][j]))
+                {
+                    frequencies[board[i][j]] = 0;
+                }
+
+                frequencies[board[i][j]] = frequencies[board[i][j]] + 1;
+            }    
+        }
+
+        var requireFrequencies = word.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count());
+
+        foreach (var letterFrequency in requireFrequencies)
+        {
+            if (!frequencies.TryGetValue(letterFrequency.Key, out int value) || value < letterFrequency.Value)
+            {
+                return false;
+            }
+        }
+
+        if (requireFrequencies[word.First()] > requireFrequencies[word.Last()])
+        {
+            word = string.Join("", word.Reverse());
+        }
+        
+        var tracked = new HashSet<string>();
+        for (var i = 0; i < board.Length; i++)
+        {
+            for (var j = 0; j < board[0].Length; j++)
+            {
+                var check = BackTrack(board, word, i, j, new HashSet<int>(), tracked);
+                if (check)
+                {
+                    return true;
+                }
+            }    
+        }
+
+        return false;
+    }
+
+    private bool BackTrack(char[][] board, string word, int x, int y, HashSet<int> visited, HashSet<string> tracked)
+    {
+        if (x < 0 || x >= board.Length || y < 0 || y >= board[0].Length)
+        {
+            return false;
+        }
+
+        var key = x * board[0].Length + y;
+
+        if (visited.Contains(key))
+        {
+            return false;
+        }
+
+        return word[0] == board[x][y] && 
+               (
+                   word.Length == 1 ||
+                   BackTrack(board, word.Substring(1), x - 1, y, new HashSet<int>([.. visited, key]), tracked) ||
+                   BackTrack(board, word.Substring(1), x + 1, y, new HashSet<int>([.. visited, key]), tracked) ||
+                   BackTrack(board, word.Substring(1), x, y - 1, new HashSet<int>([.. visited, key]), tracked) ||
+                   BackTrack(board, word.Substring(1), x, y + 1, new HashSet<int>([.. visited, key]), tracked)
+               );
+    }
+}
---
+++ src/ConsoleApp/Problems/LeetCode/WordSearch/WordSearchTest.cs
@@ -0,0 +1,82 @@
+﻿namespace ConsoleApp.Problems.LeetCode.WordSearch;
+
+public class WordSearchTest
+{
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void TestSolve(char[][] board, string word, bool expected)
+    {
+        var solution = new WordSearch();
+        var actual = solution.Solve(board, word);
+
+        Assert.Equal(expected, actual);
+    }
+
+    private static IEnumerable<object[]> TestData()
+    {
+        yield return new object[]
+        {
+            new char[][]
+            {
+                new[] { 'A', 'B', 'C', 'E' }, new [] { 'S', 'F', 'C', 'S' }, new [] { 'A', 'D', 'E', 'E' }
+            },
+            "ABCCED",
+            true
+        };
+        
+        yield return new object[]
+        {
+            new char[][]
+            {
+                new[] { 'A','B','C','E' }, new [] { 'S','F','C','S' }, new [] { 'A','D','E','E' }
+            },
+            "SEE",
+            true
+        };
+
+        yield return new object[]
+        {
+            new char[][]
+            {
+                new[] { 'A','B','C','E' }, new [] { 'S','F','C','S' }, new [] { 'A','D','E','E' }
+            },
+            "ABCB",
+            false
+        };
+        
+        yield return new object[]
+        {
+            new char[][]
+            {
+                new[] { 'a','b' }, new [] { 'c','d' }
+            },
+            "acdb",
+            true
+        };
+        
+        yield return new object[]
+        {
+            new char[][]
+            {
+                new[] { 'A','B','C','E' }, new [] { 'S','F','E','S' }, new [] { 'A','D','E','E' }
+            },
+            "ABCESEEEFS",
+            true
+        };
+        
+        yield return new object[]
+        {
+            new char[][]
+            {
+                new [] { 'A','A','A','A','A','A' }, 
+                new [] { 'A','A','A','A','A','A' }, 
+                new [] { 'A','A','A','A','A','A' },
+                new [] { 'A','A','A','A','A','A' },
+                new [] { 'A','A','A','A','A','A' },
+                new [] { 'A','A','A','A','A','A' },
+            },
+            "AAAAAAAAAAAABAA",
+            false
+        };
+    }
+}
