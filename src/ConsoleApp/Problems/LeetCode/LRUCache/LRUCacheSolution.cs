ded3a0f745d34ce8e31e18bf21f1f0bdcb34e769
Giang Pham <giang.phamhoang712@gmail.com>
2024-05-09 22:56:41 +1000

    Solution for LRU Cache

---
+++ src/ConsoleApp/Problems/LeetCode/LRUCache/LRUCacheSolution.cs
@@ -0,0 +1,107 @@
+﻿namespace ConsoleApp.Problems.LeetCode.LRUCache;
+
+public class LRUCacheSolution
+{
+    private readonly int _capacity;
+    private readonly IDictionary<int, LRUCacheItem> _data;
+    private LRUCacheItem? _leastRecent;
+    private LRUCacheItem? _mostRecent;
+
+    public LRUCacheSolution(int capacity) {
+        _capacity = capacity;
+        _data = new Dictionary<int, LRUCacheItem>();
+    }
+    
+    public int Get(int key) {
+        if (!_data.TryGetValue(key, out var item))
+        {
+            return -1;
+        }
+
+        Remove(item);
+        
+        Add(item);
+
+        return item.Value;
+    }
+    
+    public void Put(int key, int value)
+    {
+        LRUCacheItem newItem;
+        if (_data.TryGetValue(key, out var item))
+        {
+            Remove(item);
+            newItem = item;
+            item.Value = value;
+        }
+        else
+        {
+            newItem = new LRUCacheItem()
+            {
+                Key = key,
+                Value = value
+            };
+        }
+
+        if (_data.Count >= _capacity)
+        {
+            Remove(_leastRecent!);
+        }
+        
+        Add(newItem);
+    }
+
+    private void Add(LRUCacheItem item)
+    {
+        _data.Add(item.Key, item);
+
+        if (_mostRecent != null)
+        {
+            _mostRecent.Next = item;
+            item.Prev = _mostRecent;
+        }
+
+        if (_leastRecent == null)
+        {
+            _leastRecent = item;
+        }
+        
+        _mostRecent = item;
+    }
+    
+    private void Remove(LRUCacheItem item)
+    {
+        _data.Remove(item.Key);
+
+        if (item.Next != null)
+        {
+            item.Next.Prev = item.Prev;
+        }
+        
+        if (item.Prev != null)
+        {
+            item.Prev.Next = item.Next;
+        }
+
+        if (item == _leastRecent)
+        {
+            _leastRecent = item.Next;
+        }
+
+        if (item == _mostRecent)
+        {
+            _mostRecent = item.Prev;
+        }
+        
+        item.Next = null;
+        item.Prev = null;
+    }
+
+    public class LRUCacheItem
+    {
+        public int Key { get; set; }
+        public int Value { get; set; }
+        public LRUCacheItem? Prev { get; set; }
+        public LRUCacheItem? Next { get; set; }
+    }
+}
---
+++ src/ConsoleApp/Problems/LeetCode/LRUCache/LRUCacheTest.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp.Problems.LeetCode.LRUCache;
+
+public class LRUCacheTest
+{
+    [Theory]
+    [InlineData(default, default)]
+    public void TestSolve(int input, object expected)
+    {
+        var solution = new LRUCacheSolution(input);
+        
+
+        Assert.Equal(true, true);
+    }
+}
