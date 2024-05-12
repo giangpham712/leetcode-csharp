0bee020f242ef549fbfa6f47af555ab589be6b90
Giang Pham <giang.phamhoang712@gmail.com>
2024-05-12 21:20:30 +1000

    Solution for Trapping Rain Water

---
+++ src/ConsoleApp/Problems/LeetCode/TrapRainWater/TrapRainWaterSolution.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp.Problems.LeetCode.TrapRainWater;
+
+public class TrapRainWaterSolution
+{
+    public int Solve(int[] height)
+    {
+        var total = 0;
+        var left = new int[height.Length];
+        var right = new int[height.Length];
+        
+        for (var i = 0; i < height.Length; i++)
+        {
+            left[i] = i > 0 ? Math.Max(left[i - 1], height[i - 1]) : 0;
+            var j = height.Length - i - 1;
+            right[j] = j < height.Length - 1 ? Math.Max(right[j + 1], height[j + 1]) : 0;
+        }
+
+        for (var i = 0; i < height.Length; i++)
+        {
+            total += Math.Max(0, Math.Min(left[i], right[i]) - height[i]);
+        }
+
+        return total;
+    }
+}
---
+++ src/ConsoleApp/Problems/LeetCode/TrapRainWater/TrapRainWaterTest.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp.Problems.LeetCode.TrapRainWater;
+
+public class TrapRainWaterTest
+{
+    [Theory]
+    [InlineData(new int[] { 0,1,0,2,1,0,1,3,2,1,2,1 }, 6)]
+    [InlineData(new int[] { 4,2,0,3,2,5 }, 9)]
+    public void TestSolve(int[] input, int expected)
+    {
+        var solution = new TrapRainWaterSolution();
+        var actual = solution.Solve(input);
+
+        Assert.Equal(expected, actual);
+    }
+}
