eea124acdaafaa77df2ff22e6beb8735987fd299
Giang Pham <giang.phamhoang712@gmail.com>
2024-05-14 01:17:17 +1000

    Solution for String to Integer

---
+++ src/ConsoleApp/Problems/LeetCode/MyAtoi/MyAtoiSolution.cs
@@ -0,0 +1,54 @@
+﻿namespace ConsoleApp.Problems.LeetCode.MyAtoi;
+
+public class MyAtoiSolution
+{
+    public int Solve(string s)
+    {
+        const char plusSign = '+';
+        const char minusSign = '-';
+
+        long value = 0;
+        var digitStarted = false;
+        var signAdded = false;
+        var multiplier = 1;
+
+        foreach (var a in s.TrimStart())
+        {
+            if (!digitStarted)
+            {
+                if (a == plusSign || a == minusSign)
+                {
+                    if (signAdded)
+                    {
+                        break;
+                    }
+
+                    signAdded = true;
+                    multiplier = (a == plusSign ? 1 : -1);
+                    continue;
+                }
+            }
+
+            if (!(a >= '0' && a <= '9'))
+            {
+                break;
+            }
+
+            digitStarted = true;
+
+            value = value * 10 + (int) char.GetNumericValue(a);
+
+            if (value * multiplier > int.MaxValue)
+            {
+                return int.MaxValue;
+            }
+
+            if (value * multiplier < int.MinValue)
+            {
+                return int.MinValue;
+            }
+        }
+
+        return multiplier * (int)value;
+    }
+}
---
+++ src/ConsoleApp/Problems/LeetCode/MyAtoi/MyAtoiTest.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp.Problems.LeetCode.MyAtoi;
+
+public class MyAtoiTest
+{
+    [Theory]
+    [InlineData("42", 42)]
+    [InlineData(" -042", -42)]
+    [InlineData("1337c0d3", 1337)]
+    [InlineData("0-1", 0)]
+    [InlineData("words and 987", 0)]
+    public void TestSolve(string input, int expected)
+    {
+        var solution = new MyAtoiSolution();
+        var actual = solution.Solve(input);
+
+        Assert.Equal(expected, actual);
+    }
+
+    private static IEnumerable<object[]> TestData()
+    {
+        yield return new object[]
+        {
+        };
+    }
+}
