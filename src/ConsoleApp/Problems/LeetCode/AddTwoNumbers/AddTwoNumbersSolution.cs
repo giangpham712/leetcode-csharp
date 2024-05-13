fde3fdd2753e757caad8075e81ccc9a837d3f172
Giang Pham <giang.phamhoang712@gmail.com>
2024-05-13 16:33:24 +1000

    Solution for Add Two Numbers

---
+++ src/ConsoleApp/Problems/LeetCode/AddTwoNumbers/AddTwoNumbersSolution.cs
@@ -0,0 +1,57 @@
+﻿namespace ConsoleApp.Problems.LeetCode.AddTwoNumbers;
+
+public class AddTwoNumbersSolution
+{
+    public ListNode Solve(ListNode l1, ListNode l2)
+    {
+        ListNode? result = new ListNode();
+        ListNode? current = result;
+        var over = false;
+        while (l1 != null || l2 != null) {
+            
+            var sum = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + (over ? 1 : 0);
+            over = sum > 9;
+
+            current.val = sum % 10;
+            
+            l1 = l1 == null ? null : l1.next;
+            l2 = l2 == null ? null : l2.next;
+
+            if (l1 != null || l2 != null)
+            {
+                current.next = new ListNode();
+                current = current.next;
+            }
+        }
+        
+        if (over) {
+            current.next = new ListNode(1);
+        }
+        
+        return result;
+    }
+}
+
+
+ public class ListNode {
+     public int val { get; set; }
+     public ListNode? next { get; set; }
+     public ListNode(int val = 0, ListNode? next = null) {
+         this.val = val;
+         this.next = next;
+     }
+
+     public string ToString()
+     {
+         var values = new List<int>();
+
+         var current = this;
+         while (current != null)
+         {
+            values.Add(current.val);
+             current = current.next;
+         }
+
+         return $"[{string.Join(",", values)}]";
+     }
+ }
---
+++ src/ConsoleApp/Problems/LeetCode/AddTwoNumbers/AddTwoNumbersTest.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp.Problems.LeetCode.AddTwoNumbers;
+
+public class ProblemTest
+{
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void AddTwoNumbersTest(ListNode l1, ListNode l2, ListNode expected)
+    {
+        var solution = new AddTwoNumbersSolution();
+        var actual = solution.Solve(l1, l2);
+
+        Assert.Equal(expected.ToString(), actual.ToString());
+    }
+
+    private static IEnumerable<object[]> TestData()
+    {
+        yield return new object[]
+        {
+            new ListNode { val = 2, next = new ListNode { val = 4, next = new ListNode { val = 3 }}},
+            new ListNode { val = 5, next = new ListNode { val = 6, next = new ListNode { val = 4 }}},
+            new ListNode { val = 7, next = new ListNode { val = 0, next = new ListNode { val = 8 }}},
+        };
+    }
+}
