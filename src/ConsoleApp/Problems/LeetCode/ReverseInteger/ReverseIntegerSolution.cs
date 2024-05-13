e7ea2b151c4ec11712c074ed73f55e309677643a
Giang Pham <giang.phamhoang712@gmail.com>
2024-05-14 00:05:53 +1000

    Solution for Reverse Integer

---
+++ src/ConsoleApp/Problems/LeetCode/ReverseInteger/ReverseIntegerSolution.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp.Problems.LeetCode.ReverseInteger;
+
+public class ReverseIntegerSolution
+{
+    public int Solve(int x)
+    {
+        if (x == int.MinValue)
+        {
+            return 0;
+        }
+        
+        var result = 0;
+        var negative = x < 0;
+        x = negative ? x * -1 : x;
+        while (x > 0)
+        {   
+            if (result > int.MaxValue / 10)
+            {
+                return 0;
+            }
+            
+            result = result * 10 + (x % 10);
+            
+            x = x / 10;
+        }
+
+        return negative ? -result : result;
+    }
+}
---
+++ src/ConsoleApp/Problems/LeetCode/ReverseInteger/ReverseIntegerTest.cs
@@ -0,0 +1,26 @@
+﻿namespace ConsoleApp.Problems.LeetCode.ReverseInteger;
+
+public class ReverseIntegerTest
+{
+    [Theory]
+    [InlineData(123, 321)]
+    [InlineData(-123, -321)]
+    [InlineData(120, 21)]
+    [InlineData(1534236469, 0)]
+    [InlineData(-2147483412, -2143847412)]
+    [InlineData(-2147483648, 0)]
+    public void TestSolve(int input, int expected)
+    {
+        var solution = new ReverseIntegerSolution();
+        var actual = solution.Solve(input);
+
+        Assert.Equal(expected, actual);
+    }
+
+    private static IEnumerable<object[]> TestData()
+    {
+        yield return new object[]
+        {
+        };
+    }
+}
