e6fe39bb126faffd614fbb2fb3f5c77cbe8022f5
Giang Pham <giang.phamhoang712@gmail.com>
2024-05-23 21:53:50 +1000

    Solution for Minimum Remove to Make Valid Parentheses

---
+++ src/ConsoleApp/Problems/LeetCode/MinimumRemoveToMakeValidParentheses/MinimumRemoveToMakeValidParenthesesSolution.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics;
+using System.Text;
+
+namespace ConsoleApp.Problems.LeetCode.MinimumRemoveToMakeValidParentheses;
+
+public class MinimumRemoveToMakeValidParenthesesSolution
+{
+    public object Solve(string s)
+    {
+        var cursor = 0;
+        var arrayStack = new int[s.Length];
+
+        var toRemoves = new HashSet<int>();
+        
+        for (var i = 0; i < s.Length; i++)
+        {
+            var c = s[i];
+            if (c != '(' && c != ')')
+            {
+                continue;
+            }
+
+            if (c == ')')
+            {
+                if (cursor > 0 && s[arrayStack[cursor - 1]] == '(')
+                {
+                    cursor--;
+                }
+                else
+                {
+                    toRemoves.Add(i);
+                }
+            }
+            else
+            {
+                arrayStack[cursor] = i;
+                cursor++;
+            }
+        }
+
+        foreach (var c in arrayStack.Take(cursor))
+        {
+            toRemoves.Add(c);
+        }
+
+        var result = new StringBuilder();
+        for (var i = 0; i < s.Length; i++)
+        {
+            if (!toRemoves.Contains(i))
+            {
+                result.Append(s[i]);
+            }
+        }
+
+        return result.ToString();
+    }
+}
---
+++ src/ConsoleApp/Problems/LeetCode/MinimumRemoveToMakeValidParentheses/MinimumRemoveToMakeValidParenthesesSolution2.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp.Problems.LeetCode.MinimumRemoveToMakeValidParentheses;
+
+public class MinimumRemoveToMakeValidParenthesesSolution2
+{
+    public string Solve(string s)
+    {
+        var leftParenCount = 0;
+
+        foreach (var c in s)
+        {
+            
+        }
+
+        return "";
+    }
+}
