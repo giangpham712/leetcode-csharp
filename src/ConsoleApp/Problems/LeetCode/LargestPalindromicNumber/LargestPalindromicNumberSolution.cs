f4a3354a59b82521ed2080f017abf3f1a9524a41
Giang Pham <giang.phamhoang712@gmail.com>
2024-05-25 01:26:42 +1000

    Solution for Largest Palindromic Number

---
+++ src/ConsoleApp/Problems/LeetCode/LargestPalindromicNumber/LargestPalindromicNumberSolution.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+
+namespace ConsoleApp.Problems.LeetCode.LargestPalindromicNumber;
+
+public class LargestPalindromicNumberSolution
+{
+    public string Solve(string num)
+    {
+        var frequencies = new Dictionary<char, int>();
+
+        foreach (var c in num)
+        {
+            if (frequencies.ContainsKey(c))
+            {
+                frequencies[c] += 1;
+            }
+            else
+            {
+                frequencies[c] = 1;
+            }
+        }
+
+        var sorted = frequencies.Keys.OrderDescending().ToList();
+
+        var nonZero = false;
+        var left = new StringBuilder();
+        foreach (var digit in sorted)
+        {
+            if (frequencies[digit] < 2)
+            {
+                continue;
+            }
+            
+            if (digit != '0')
+            {
+                nonZero = true;
+            }
+
+            if (!nonZero)
+            {
+                continue;
+            }
+            
+            while (frequencies[digit] >= 2)
+            {
+                left.Append(digit);
+                frequencies[digit] -= 2;
+            }
+        }
+
+        var sortedRemainingDigits = sorted.Where(x => frequencies[x] > 0).ToArray();
+        var mid = sortedRemainingDigits.Any()
+            ? sortedRemainingDigits[0].ToString()
+            : ""; 
+
+        var result = left.ToString();
+        return (result + mid + string.Join("", result.Reverse()));
+    }
+}
---
+++ src/ConsoleApp/Problems/LeetCode/LargestPalindromicNumber/LargestPalindromicNumberTest.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp.Problems.LeetCode.LargestPalindromicNumber;
+
+public class LargestPalindromicNumberTest
+{
+    [Theory]
+    [InlineData("444947137", "7449447")]
+    [InlineData("00009", "9")]
+    public void TestSolve(string input, string expected)
+    {
+        var solution = new LargestPalindromicNumberSolution();
+        var actual = solution.Solve(input);
+
+        Assert.Equal(expected, actual);
+    }
+
+    private static IEnumerable<object[]> TestData()
+    {
+        yield return new object[]
+        {
+        };
+    }
+}
